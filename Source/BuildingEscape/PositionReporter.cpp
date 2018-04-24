e08356f0ea3ee6e3dc01e8835d391ee5c02ecedf
Pedro Henriques <pdhenriques@gmail.com>
2018-04-24 10:49:08 +0200

    Add log feature to Position Reporter class

--- Source/BuildingEscape/PositionReporter.cpp
+++ Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -20,7 +21,8 @@ void UPositionReporter::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting position!"));
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s"), *ObjectName);
 }
 
 
